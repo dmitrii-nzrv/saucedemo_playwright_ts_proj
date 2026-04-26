54d2e46a14bae97de4237a5f59494d130ba257f5
Dmitrii <dmitrii.nazarov7703@gmail.com>
2026-04-26 03:23:04 +0300

    implement basic POM

---
+++ locators/LoginLocators.ts
@@ -0,0 +1,5 @@
+export const LoginLocators = {
+  userNameInput: '#user-name',
+  userPasswordInput: '#password',
+  loginButton: '#login-button',
+};
---
+++ pages/LoginPage.ts
@@ -0,0 +1,12 @@
+import { LoginLocators } from '../locators/LoginLocators';
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  constructor(private page: Page) {}
+
+  async login(username: string, password: string) {
+    await this.page.fill(LoginLocators.userNameInput, username);
+    await this.page.fill(LoginLocators.userPasswordInput, password);
+    await this.page.click(LoginLocators.loginButton);
+  }
+}
